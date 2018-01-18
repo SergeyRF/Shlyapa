8b005689e3b7bce4f4262757e3436d46e13d48cd
volgoak <volgoak@gmail.com>
2018-01-18 18:45:25 +0300

    Swipe listener created

--- app/src/main/java/com/example/sergey/shlyapa/GameActyvity.java
+++ app/src/main/java/com/example/sergey/shlyapa/GameActyvity.java
@@ -2,12 +2,46 @@ package com.example.sergey.shlyapa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class GameActyvity extends AppCompatActivity {
+
+    private TextView tvWord;
+    private CardView cvWord;
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        tvWord = findViewById(R.id.tv_word);
+        cvWord = findViewById(R.id.cv_word);
+
+        game = new Game();
+
+        tvWord.setText(game.getNextWord());
+        cvWord.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        cvWord.setOnTouchListener(new OnSwipeTouchListener() {
+            @Override
+            public boolean onSwipeLeft() {
+                Toast.makeText(GameActyvity.this, "Swipe left", Toast.LENGTH_LONG).show();
+                return true;
+            }
+
+            @Override
+            public boolean onSwipeRight() {
+                Toast.makeText(GameActyvity.this, "Swipe right", Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
     }
 }
